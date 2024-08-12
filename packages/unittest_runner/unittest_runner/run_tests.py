03d0cfa7cac11cb9aa29f99bcc51857634f8b6e3
Molly Moen <molly@code.org>
2024-08-12 14:11:29 -0700

    return simplified results array

--- packages/unittest_runner/unittest_runner/run_tests.py
+++ packages/unittest_runner/unittest_runner/run_tests.py
@@ -4,15 +4,17 @@ from .validation_runner import ValidationTestResult
 # Run the tests in the given file pattern and display the results to the user.
 # Validation tests use the ValidationTestResult class to display only the short description of the test.
 def run_validation_tests(file_pattern):
-  run_tests(file_pattern, ValidationTestResult)
+  result = run_tests(file_pattern, ValidationTestResult)
+  return result.simplified_results
 
 # Run the tests in the given file pattern and display the results to the user.
 # Student tests use the standard TextTestResult class to display the test name and short description.
 def run_student_tests(file_pattern):
-  run_tests(file_pattern, unittest.TextTestResult)
+  return run_tests(file_pattern, unittest.TextTestResult)
 
 def run_tests(file_pattern, resultclass):
   loader = unittest.TestLoader()
   test_suite = loader.discover('.', file_pattern)
   runner = unittest.TextTestRunner(verbosity=2, resultclass=resultclass)
   result = runner.run(test_suite)
+  return result
--- packages/unittest_runner/unittest_runner/validation_runner.py
+++ packages/unittest_runner/unittest_runner/validation_runner.py
@@ -2,6 +2,9 @@
 from unittest import TextTestResult
 
 class ValidationTestResult(TextTestResult):
+  simplified_results = []
+
+
   # The default behavior is to display the test name and short description.
   # We want to display only the short description if it exists, otherwise just the test name.
   # Students don't see the tests, so we don't need to display the test name.
@@ -12,3 +15,27 @@ class ValidationTestResult(TextTestResult):
       return doc_first_line
     else:
       return str(test)
+
+  def addSuccess(self, test):
+      super(ValidationTestResult, self).addSuccess(test)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "PASS"})
+
+  def addError(self, test, err):
+      super(ValidationTestResult, self).addError(test, err)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "ERROR"})
+
+  def addFailure(self, test, err):
+      super(ValidationTestResult, self).addFailure(test, err)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "FAIL"})
+
+  def addSkip(self, test, reason):
+      super(ValidationTestResult, self).addSkip(test, reason)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "SKIP"})
+
+  def addExpectedFailure(self, test, err):
+      super(ValidationTestResult, self).addExpectedFailure(test, err)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "EXPECTED_FAILURE"})
+
+  def addUnexpectedSuccess(self, test):
+      super(ValidationTestResult, self).addUnexpectedSuccess(test)
+      self.simplified_results.append({'name': self.getDescription(test), 'result': "UNEXPECTED_SUCCESS"})
