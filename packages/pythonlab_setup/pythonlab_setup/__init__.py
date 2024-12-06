169fd2747925ddbad95afa753cdfff0a6c059ac5
Molly Moen <molly@code.org>
2024-12-06 13:41:15 -0800

    fix linter errors

--- packages/pythonlab_setup/pythonlab_setup/__init__.py
+++ packages/pythonlab_setup/pythonlab_setup/__init__.py
@@ -1,2 +1,2 @@
-from .setup_pythonlab import setup_pythonlab
-from .teardown_pythonlab import teardown_pythonlab
+from .setup_pythonlab import setup_pythonlab as setup_pythonlab
+from .teardown_pythonlab import teardown_pythonlab  as teardown_pythonlab
--- packages/unittest_runner/unittest_runner/__init__.py
+++ packages/unittest_runner/unittest_runner/__init__.py
@@ -1,1 +1,1 @@
-from .run_tests import run_student_tests, run_validation_tests
+from .run_tests import run_student_tests as run_student_tests, run_validation_tests as run_validation_tests
